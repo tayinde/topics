7ada335ff71ef2f3d627fc4475d59aea55ab8013
tayinde <tbiayinde@gmail.com>
2020-09-30 22:36:58 -0400

    ability to toggle preview

--- wwwroot/js/create-topic.js
+++ wwwroot/js/create-topic.js
@@ -4,11 +4,22 @@ import {renderTags} from './tag_renderer.js'
 $('.username').attr('value', user);
 $('.token').attr('value', token);
 
+var previewToggled = false
+//import 'jquery'
 $('#preview-button').click(() => {
-	$('#preview').show()
-	$('#preview-content').html($('#content').val())
-	$('#preview-title').html($('#title').val())
-	$('#preview-author').html(user)
-	$('#pfp').css('background-image', `url('${localStorage.getItem('profile_picture')}')`)
-	renderTags('#preview-content')
+	if (!previewToggled) {
+		$('html, body').animate({ scrollTop: 430}, 10);
+		$('#preview').show()
+		$('#preview-button').html("Hide preview")
+		$('#preview-content').html($('#content').val())
+		$('#preview-title').html($('#title').val())
+		$('#preview-author').html(user)
+		$('#pfp').css('background-image', `url('${localStorage.getItem('profile_picture')}')`)
+		renderTags('#preview-content')
+		previewToggled = true
+	} else {
+		$('#preview').hide()
+		$('#preview-button').html("Preview")
+		previewToggled = false
+	}
 })
