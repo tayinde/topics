45d6f4a520aab09c84a344fa4045953eba1da315
tayinde <tbiayinde@gmail.com>
2020-09-30 22:22:48 -0400

    previews for posts

--- wwwroot/js/create-topic.js
+++ wwwroot/js/create-topic.js
@@ -1,4 +1,14 @@
-user = localStorage.getItem('user') || null;
-token = localStorage.getItem('token') || null;
+var user = localStorage.getItem('user') || null;
+var token = localStorage.getItem('token') || null;
+import {renderTags} from './tag_renderer.js'
 $('.username').attr('value', user);
 $('.token').attr('value', token);
+
+$('#preview-button').click(() => {
+	$('#preview').show()
+	$('#preview-content').html($('#content').val())
+	$('#preview-title').html($('#title').val())
+	$('#preview-author').html(user)
+	$('#pfp').css('background-image', `url('${localStorage.getItem('profile_picture')}')`)
+	renderTags('#preview-content')
+})
